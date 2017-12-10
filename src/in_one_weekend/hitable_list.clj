2e89f89b7bacf3ccf3c0a3d5eb8022ed17842a2c
iyahoo <s1200191@gmail.com>
2017-12-10 23:15:49 +0900

    :hammer: [Fix] functional

--- src/in_one_weekend/hitable_list.clj
+++ src/in_one_weekend/hitable_list.clj
@@ -1,23 +1,28 @@
 (ns in-one-weekend.hitable-list
-  (:require [in-one-weekend.vec :refer :all]
-            [in-one-weekend.ray :refer :all]
+  (:require [in-one-weekend.ray :refer :all]
             [in-one-weekend.hitable :refer :all]
-            [in-one-weekend.sphere :refer :all]))
-
-(defn hit [this ray t-min t-max]
-  (loop [i 0
-         temp-rec nil
-         hit-anything false
-         closest-so-far t-max]
-    (if (< i (:list-size this))
-      (let [list-i (nth (:lis this) i)
-            return-hit (hit-sphere list-i ray t-min closest-so-far)
-            return-rec (:rec return-hit)]
-        (if (return-hit :result)
-          (recur (+ i 1) return-rec true         (:t return-rec))
-          (recur (+ i 1) temp-rec   hit-anything closest-so-far)))
-      {:result hit-anything :rec temp-rec})))
+            [in-one-weekend.sphere :refer [hit-sphere]]))
 
 (defrecord Hitable-list [lis list-size])
 
+;; (defn hit [hitlis ray t-min t-max]
+;;   {:pre [(= (class hitlis) Hitable-list)]}
+;;   (loop [i 0
+;;          temp-rec nil
+;;          hit-anything false
+;;          closest-so-far t-max]
+;;     (if (< i (:list-size hitlis))
+;;       (let [list-i (nth (:lis hitlis) i)
+;;             {:keys [result rec]} (hit-sphere list-i ray t-min closest-so-far)]
+;;         (if result
+;;           (recur (+ i 1) rec      true         (:t rec))
+;;           (recur (+ i 1) temp-rec hit-anything closest-so-far)))
+;;       {:result hit-anything :rec temp-rec})))
 
+(defn hit [hitlis ray t-min t-max]
+  {:pre [(= (class hitlis) Hitable-list)]}
+  (->> (range (:list-size hitlis))
+       (map #(hit-sphere (nth (:lis hitlis) %) ray t-min t-max))
+       (filter :rec)
+       (sort-by :t)
+       (first)))
