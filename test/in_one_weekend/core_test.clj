8164c864b078ca1f0c8fe39bd528890d9440975a
iyahoo <s1200191@gmail.com>
2017-12-22 17:17:27 +0900

    :white_check_mark: [Add] header, int-color

--- test/in_one_weekend/core_test.clj
+++ test/in_one_weekend/core_test.clj
@@ -9,6 +9,16 @@
 ;; 0 0 51\n85 0 51\n170 0 51\n"))))
 
 (deftest core-methods
+  (testing "header"
+    (is (= (header 1 1)
+           "P3\n1 1\n255\n")))
+  (testing "int-color"
+    (is (= (int-color [0.5 0.5 0.5])
+           [127 127 127]))
+    (is (= (int-color [0.0 0.0 0.0])
+           [0 0 0]))
+    (is (= (int-color [1.0 1.0 1.0])
+           [255 255 255])))
   (testing "make-coordinates"
     (is (= (make-coordinates 3 2)
            [[1 0] [1 1] [1 2] [0 0] [0 1] [0 2]]))))
