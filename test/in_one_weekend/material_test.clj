b176e89e525e5ef21da4ceabd5a27af095c941f3
iyahoo <s1200191@gmail.com>
2017-12-22 17:17:55 +0900

    :white_check_mark: [Add] schlick

--- test/in_one_weekend/material_test.clj
+++ test/in_one_weekend/material_test.clj
@@ -8,6 +8,15 @@
     (is (equals (reflect (->Vec3 1.0 1.0 0.0) (->Vec3 0.0 1.0 0.0))
                 (->Vec3 1.0 -1.0 0.0))))
 
+  (testing "schlick by https://www.wolframalpha.com/input/?i=((1-0.5)+%2F+(1+%2B+0.5))%5E2+%2B+(1+-+((1-0.5)+%2F+(1+%2B+0.5))%5E2)*(1-cos60)%5E5"
+    (is (close? (schlick (Math/cos (* 60 (/ Math/PI 180))) 0.5)
+                0.13888888
+                0.000001)))
+  (testing "schlick by https://www.wolframalpha.com/input/?i=((1-0.2)+%2F+(1+%2B+0.2))%5E2+%2B+(1+-+((1-0.2)+%2F+(1+%2B+0.2))%5E2)*(1-cos30)%5E5"
+    (is (close? (schlick (Math/cos (* 30 (/ Math/PI 180))) 0.2)
+                0.444468
+                0.000001)))
+
   (testing "acute-angle?"
     (is (acute-angle? (->Vec3 1 1 1) (->Vec3 10 3 1)))
     (is (not (acute-angle? (->Vec3 -1 -1 0) (->Vec3 1 1 0))))))
