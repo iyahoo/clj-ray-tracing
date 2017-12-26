62c07c044271bc6f5f959bf1318a6119c16d7e34
iyahoo <s1200191@gmail.com>
2017-12-26 15:30:20 +0900

    :white_check_mark: [Add] orthonormal-basis

--- test/in_one_weekend/camera_test.clj
+++ test/in_one_weekend/camera_test.clj
@@ -1,4 +1,19 @@
 (ns in-one-weekend.camera-test
   (:require [clojure.test :refer :all]
-            [in-one-weekend.camera :refer :all]))
+            [in-one-weekend.camera :refer :all]
+            [in-one-weekend.vec :refer :all]))
 
+(deftest cameramethod
+  (testing "orthonormal-basis"
+    (let [[w u v] (orthonormal-basis (->Vec3 1 1 0)
+                                     (->Vec3 2 -2 0)
+                                     (->Vec3 0 0 3))]
+      (are [x] (close? x 0 0.00001)
+        (dot w u)
+        (dot u v)
+        (dot w v))
+
+      (are [x] (close? x 1 0.00001)
+        (vector-length w)
+        (vector-length u)
+        (vector-length v)))))
